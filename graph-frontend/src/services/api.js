0fedc93526021f5480ea03629f25dd4b74b20a20
Siva Sai Dwarapureddy <137069575+sivasaidwarapureddy@users.noreply.github.com>
2026-05-17 02:36:03 +0530

    Frontend deploy config

--- graph-frontend/src/services/api.js
+++ graph-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://dummyjson.com",
 });
 
 export const createGraph = (data) => API.post("/graph/create", data);
