2f154151408509dcc2bd0b6af6160ab44bdce89d
Siva Sai Dwarapureddy <137069575+sivasaidwarapureddy@users.noreply.github.com>
2026-05-17 03:29:34 +0530

    Fixed CORS

--- graphvisualizer/src/main/java/com/graph/graphvisualizer/config/CorsConfig.java
+++ graphvisualizer/src/main/java/com/graph/graphvisualizer/config/CorsConfig.java
@@ -9,13 +9,18 @@ public class CorsConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
+
         return new WebMvcConfigurer() {
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*");
-                       
+                        .allowedOrigins(
+                                "https://graph-frontend-yv3f.onrender.com"
+                        )
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
             }
         };
     }
