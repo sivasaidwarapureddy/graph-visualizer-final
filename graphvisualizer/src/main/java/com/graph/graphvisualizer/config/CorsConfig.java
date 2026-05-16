1724dfd428979a9a13e87ba1b9afb6b05029cdcd
Siva Sai Dwarapureddy <137069575+sivasaidwarapureddy@users.noreply.github.com>
2026-05-17 03:07:34 +0530

    Added CORS

--- graphvisualizer/src/main/java/com/graph/graphvisualizer/config/CorsConfig.java
+++ graphvisualizer/src/main/java/com/graph/graphvisualizer/config/CorsConfig.java
@@ -13,9 +13,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
+                        .allowedOrigins("*")
+                        .allowedMethods("*");
+                       
             }
         };
     }
